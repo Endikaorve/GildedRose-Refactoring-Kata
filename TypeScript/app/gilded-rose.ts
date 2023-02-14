cd17d5f70957070a63205f54ecbc10cc56065026
Javier Gutierrez <javier.gutierrez@540deg.com>
2023-02-14 17:47:22 +0100

    Remplazar for con forEach y crear condiciones para cada item

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,105 +18,103 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      const productName = this.items[i].name
-      // objeto Sulfuras
-      if (productName === 'Sulfuras, Hand of Ragnaros') {
-        continue
+    this.items.forEach(item => {
+      const productName = item.name
+
+      switch (productName) {
+        case 'Sulfuras, Hand of Ragnaros':
+          break
+        case 'Aged Brie':
+          this.upgradeAgedBrieQuality(item)
+          break
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          this.upgradeBackStagePassQuality(item)
+          break
+        default:
+          this.upgradeOtherItemsQuality(item)
       }
-
-      this.decreaseSellInDate(i)
-
-      // Item concreto Aged Brie
-      if (productName === 'Aged Brie') {
-        this.upgradeAgedBrieQuality(i)
-        continue
-      }
-
-      // Item concreto Backstage passes to a TAFKAL80ETC concert
-      if (productName === 'Backstage passes to a TAFKAL80ETC concert') {
-        this.upgradeBackStagePassQuality(i)
-        continue
-      }
-
-      // Resto de items
-      this.upgradeOtherItemsQuality(i)
-    }
+    })
 
     return this.items
   }
 
-  private increaseQuality(i: number) {
-    this.items[i].quality += 1
+  private increaseQuality(item: Item) {
+    item.quality += 1
   }
 
-  private decreaseQuality(i: number) {
-    this.items[i].quality -= 1
+  private decreaseQuality(item: Item) {
+    item.quality -= 1
   }
 
-  private decreaseSellInDate(i: number) {
-    this.items[i].sellIn -= 1
+  private decreaseSellInDate(item: Item) {
+    item.sellIn -= 1
   }
 
-  private hasMaxQuality(i: number) {
-    return this.items[i].quality === 50
+  private hasMaxQuality(item: Item) {
+    return item.quality === 50
   }
 
-  private hasExpiredSellInDate(i: number) {
-    return this.items[i].sellIn < 0
+  private hasExpiredSellInDate(item: Item) {
+    return item.sellIn < 0
   }
 
-  private upgradeBackStagePassQuality(i: number) {
-    if (this.hasMaxQuality(i)) {
+  private upgradeBackStagePassQuality(item: Item) {
+    this.decreaseSellInDate(item)
+
+    if (this.hasMaxQuality(item)) {
       return
     }
 
-    if (this.hasExpiredSellInDate(i)) {
-      this.items[i].quality = 0
+    if (this.hasExpiredSellInDate(item)) {
+      item.quality = 0
       return
     }
 
     if (this.items[0].sellIn < 6) {
-      this.increaseQuality(i)
-      this.increaseQuality(i)
-      this.increaseQuality(i)
+      this.increaseQuality(item)
+      this.increaseQuality(item)
+      this.increaseQuality(item)
       return
     }
 
     if (this.items[0].sellIn < 11) {
-      this.increaseQuality(i)
-      this.increaseQuality(i)
+      this.increaseQuality(item)
+      this.increaseQuality(item)
       return
     }
 
-    this.increaseQuality(i)
+    this.increaseQuality(item)
   }
 
-  private upgradeAgedBrieQuality(i: number) {
-    if (this.hasMaxQuality(i)) {
+  private upgradeAgedBrieQuality(item: Item) {
+    this.decreaseSellInDate(item)
+
+    if (this.hasMaxQuality(item)) {
       return
     }
 
-    if (this.hasExpiredSellInDate(i)) {
-      this.increaseQuality(i)
-      this.increaseQuality(i)
+    if (this.hasExpiredSellInDate(item)) {
+      this.increaseQuality(item)
+      this.increaseQuality(item)
       return
     }
 
-    this.increaseQuality(i)
+    this.increaseQuality(item)
   }
 
-  private upgradeOtherItemsQuality(i: number) {
-    if (this.hasMaxQuality(i)) {
+  private upgradeOtherItemsQuality(item: Item) {
+    this.decreaseSellInDate(item)
+
+    if (this.hasMaxQuality(item)) {
       return
     }
 
-    if (this.items[i].quality > 0) {
-      if (this.hasExpiredSellInDate(i)) {
-        this.decreaseQuality(i)
+    if (item.quality > 0) {
+      if (this.hasExpiredSellInDate(item)) {
+        this.decreaseQuality(item)
       }
 
-      this.decreaseQuality(i)
+      this.decreaseQuality(item)
     }
   }
 }
