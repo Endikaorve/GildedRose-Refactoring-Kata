9ef5a514d7f0d77e093ccc58bac8ab646e73eb03
Javier Gutierrez <javier.gutierrez@540deg.com>
2023-02-14 13:24:20 +0100

    Extraer lógicas ifs y naming variables

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,69 +1,83 @@
 export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+  name: string
+  sellIn: number
+  quality: number
 
   constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
+    this.name = name
+    this.sellIn = sellIn
+    this.quality = quality
   }
 }
 
 export class GildedRose {
-  items: Array<Item>;
+  items: Array<Item>
 
   constructor(items = [] as Array<Item>) {
-    this.items = items;
+    this.items = items
   }
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+      const itemName = this.items[i].name
+      let itemQuality = this.items[i].quality
+      let itemSellInDate = this.items[i].sellIn
+
+      // objeto Sulfuras
+      if (itemName === 'Sulfuras, Hand of Ragnaros') {
+        break
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+
+      itemSellInDate = itemSellInDate - 1
+
+      // Calidad mayor de 50 de cualquier objeto
+      if (itemQuality >= 50) {
+        break
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
+
+      // Item concreto Aged Brie
+      if (itemName === 'Aged Brie') {
+        if (itemSellInDate < 0) {
+          itemQuality = itemQuality + 1
         }
+
+        itemQuality = itemQuality + 1
+
+        this.items[i].quality = itemQuality
+        this.items[i].sellIn = itemSellInDate
+        break
+      }
+
+      // Item concreto Backstage passes to a TAFKAL80ETC concert
+      if (itemName === 'Backstage passes to a TAFKAL80ETC concert') {
+        itemQuality = itemQuality + 1
+        if (itemSellInDate < 11) {
+          itemQuality = itemQuality + 1
+        }
+        if (itemSellInDate < 6) {
+          itemQuality = itemQuality + 1
+        }
+
+        if (itemSellInDate <= 0) {
+          itemQuality = 0
+        }
+
+        this.items[i].quality = itemQuality
+        this.items[i].sellIn = itemSellInDate
+        break
+      }
+
+      if (itemQuality > 0) {
+        if (itemSellInDate < 0) {
+          itemQuality = itemQuality - 1
+        }
+
+        itemQuality = itemQuality - 1
+        this.items[i].quality = itemQuality
+        this.items[i].sellIn = itemSellInDate
       }
     }
 
-    return this.items;
+    return this.items
   }
 }
