c8f336236f20fdf757995cd31594b13f36d85a7e
Endika Orube <endika.orube@biko2.com>
2023-05-12 13:10:06 +0200

    refactor: elimina función updateQuality

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -20,7 +20,54 @@ export class GildedRose {
       }
 
       updateSellIn(item);
-      updateQuality(item);
+
+      if (item.name === "Aged Brie") {
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 0) {
+          item.quality = item.quality + 1;
+        }
+
+        if (item.quality > 50) {
+          item.quality = 50;
+        }
+
+        return;
+      }
+
+      if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.sellIn < 0) {
+          item.quality = 0;
+
+          return;
+        }
+
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 11) {
+          item.quality = item.quality + 1;
+        }
+        if (item.sellIn < 6) {
+          item.quality = item.quality + 1;
+        }
+
+        if (item.quality > 50) {
+          item.quality = 50;
+        }
+
+        return;
+      }
+
+      // Item común
+      item.quality = item.quality - 1;
+
+      if (item.sellIn < 0) {
+        item.quality = item.quality - 1;
+      }
+
+      if (item.quality < 0) {
+        item.quality = 0;
+      }
     });
 
     return this.items;
@@ -29,54 +76,4 @@ export class GildedRose {
 
 const updateSellIn = (item: Item) => {
   item.sellIn = item.sellIn - 1;
-};
-
-const updateQuality = (item: Item) => {
-  if (item.name === "Aged Brie") {
-    item.quality = item.quality + 1;
-
-    if (item.sellIn < 0) {
-      item.quality = item.quality + 1;
-    }
-
-    if (item.quality > 50) {
-      item.quality = 50;
-    }
-
-    return;
-  }
-
-  if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
-    if (item.sellIn < 0) {
-      item.quality = 0;
-
-      return;
-    }
-
-    item.quality = item.quality + 1;
-
-    if (item.sellIn < 11) {
-      item.quality = item.quality + 1;
-    }
-    if (item.sellIn < 6) {
-      item.quality = item.quality + 1;
-    }
-
-    if (item.quality > 50) {
-      item.quality = 50;
-    }
-
-    return;
-  }
-
-  // Item común
-  item.quality = item.quality - 1;
-
-  if (item.sellIn < 0) {
-    item.quality = item.quality - 1;
-  }
-
-  if (item.quality < 0) {
-    item.quality = 0;
-  }
 };
