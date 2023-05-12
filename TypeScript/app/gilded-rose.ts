be751d3a78f47552aa0d205f5bfee168fa1609fb
Endika Orube <endika.orube@biko2.com>
2023-05-12 12:51:44 +0200

    refactor: separa Aged Brie de Backstage

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -50,29 +50,32 @@ const updateSellIn = (item: Item) => {
 };
 
 const updateQuality1 = (item: Item) => {
-  if (
-    item.name === "Aged Brie" ||
-    item.name === "Backstage passes to a TAFKAL80ETC concert"
-  ) {
+  if (item.name === "Aged Brie") {
     if (item.quality < 50) {
       item.quality = item.quality + 1;
-      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
     }
 
     return;
   }
 
+  if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
+    item.quality = item.quality + 1;
+
+    if (item.sellIn < 11) {
+      item.quality = item.quality + 1;
+    }
+    if (item.sellIn < 6) {
+      item.quality = item.quality + 1;
+    }
+
+    if (item.quality > 50) {
+      item.quality = 50;
+    }
+
+    return;
+  }
+
+  // Item común
   if (item.quality > 0) {
     item.quality = item.quality - 1;
   }
