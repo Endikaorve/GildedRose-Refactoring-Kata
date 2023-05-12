76214213efeda59b92c725871818e6e7c892faa3
Endika Orube <endika.orube@biko2.com>
2023-05-12 13:14:57 +0200

    refactor: extrae Aged Brie

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -16,21 +16,12 @@ export class GildedRose {
   updateQuality() {
     this.items.forEach((item) => {
       if (item.name === "Sulfuras, Hand of Ragnaros") {
+        this.updateSulfuras();
         return;
       }
 
       if (item.name === "Aged Brie") {
-        item.sellIn = item.sellIn - 1;
-
-        item.quality = item.quality + 1;
-
-        if (item.sellIn < 0) {
-          item.quality = item.quality + 1;
-        }
-
-        if (item.quality > 50) {
-          item.quality = 50;
-        }
+        this.updateAgedBrie(item);
 
         return;
       }
@@ -75,5 +66,21 @@ export class GildedRose {
     });
 
     return this.items;
+  }
+
+  private updateSulfuras() {}
+
+  private updateAgedBrie(item: Item) {
+    item.sellIn = item.sellIn - 1;
+
+    item.quality = item.quality + 1;
+
+    if (item.sellIn < 0) {
+      item.quality = item.quality + 1;
+    }
+
+    if (item.quality > 50) {
+      item.quality = 50;
+    }
   }
 }
