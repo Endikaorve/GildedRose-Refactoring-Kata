67cd56ca03352922becf2551ed3166d0b8af548d
Endika Orube <endika.orube@biko2.com>
2023-05-12 12:42:49 +0200

    refactor: extrae updateQuality (primera parte) a función

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -16,33 +16,7 @@ export class GildedRose {
   updateQuality() {
     this.items.forEach((item) => {
       updateSellIn(item);
-
-      if (
-        item.name != "Aged Brie" &&
-        item.name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
-        }
-      }
+      updateQuality1(item);
 
       if (item.sellIn < 0) {
         if (item.name != "Aged Brie") {
@@ -70,5 +44,34 @@ export class GildedRose {
 const updateSellIn = (item: Item) => {
   if (item.name != "Sulfuras, Hand of Ragnaros") {
     item.sellIn = item.sellIn - 1;
+  }
+};
+
+const updateQuality1 = (item: Item) => {
+  if (
+    item.name != "Aged Brie" &&
+    item.name != "Backstage passes to a TAFKAL80ETC concert"
+  ) {
+    if (item.quality > 0) {
+      if (item.name != "Sulfuras, Hand of Ragnaros") {
+        item.quality = item.quality - 1;
+      }
+    }
+  } else {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+      }
+    }
   }
 };
