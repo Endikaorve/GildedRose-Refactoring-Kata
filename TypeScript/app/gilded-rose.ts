5cf8530bd5271bd764a7d54409c6486db2968118
Endika Orube <endika.orube@biko2.com>
2023-05-12 12:52:53 +0200

    refactor: extrae updateQuality (segunda parte)

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,24 +21,7 @@ export class GildedRose {
 
       updateSellIn(item);
       updateQuality1(item);
-
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
+      updateQuality2(item);
     });
 
     return this.items;
@@ -78,5 +61,25 @@ const updateQuality1 = (item: Item) => {
   // Item común
   if (item.quality > 0) {
     item.quality = item.quality - 1;
+  }
+};
+
+const updateQuality2 = (item: Item) => {
+  if (item.sellIn < 0) {
+    if (item.name != "Aged Brie") {
+      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.quality = item.quality - 1;
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
   }
 };
