f84211f85758e55baaab9050fceb97c4b807536e
Endika Orube <endika.orube@biko2.com>
2023-05-12 12:45:49 +0200

    refactor: elimina lógica de Sulfuras inservible

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -55,9 +55,7 @@ const updateQuality1 = (item: Item) => {
     item.name != "Backstage passes to a TAFKAL80ETC concert"
   ) {
     if (item.quality > 0) {
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.quality = item.quality - 1;
-      }
+      item.quality = item.quality - 1;
     }
   } else {
     if (item.quality < 50) {
