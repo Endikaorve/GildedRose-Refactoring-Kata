fc1caa156d231ad1604dab462d7325e85307d20c
Endika Orube <endika.orube@biko2.com>
2023-05-12 12:55:12 +0200

    refactor: invierte if de Aged Brie

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -69,17 +69,19 @@ const updateQuality2 = (item: Item) => {
     return;
   }
 
-  if (item.name != "Aged Brie") {
-    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1;
-      }
-    } else {
-      item.quality = item.quality - item.quality;
-    }
-  } else {
+  if (item.name === "Aged Brie") {
     if (item.quality < 50) {
       item.quality = item.quality + 1;
     }
+
+    return;
+  }
+
+  if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
+  } else {
+    item.quality = item.quality - item.quality;
   }
 };
