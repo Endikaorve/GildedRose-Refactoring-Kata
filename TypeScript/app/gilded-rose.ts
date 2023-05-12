53b323d6c3345a1e0f44979d1d805d32a5ee40fa
Endika Orube <endika.orube@biko2.com>
2023-05-12 13:07:55 +0200

    refactor: mejora lógica de mínimo en item común

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -70,15 +70,13 @@ const updateQuality = (item: Item) => {
   }
 
   // Item común
-  if (item.quality > 0) {
+  item.quality = item.quality - 1;
+
+  if (item.sellIn < 0) {
     item.quality = item.quality - 1;
   }
 
-  if (item.sellIn >= 0) {
-    return;
-  }
-
-  if (item.quality > 0) {
-    item.quality = item.quality - 1;
+  if (item.quality < 0) {
+    item.quality = 0;
   }
 };
