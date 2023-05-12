3115d38c601e07572c171f62adc4c03fb6543c74
Endika Orube <endika.orube@biko2.com>
2023-05-12 11:57:54 +0200

    chore: añade solución chatgpt

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -20,4 +20,35 @@ export class GildedRose {
 
     return this.items
   }
+
+  // CHAT-GPT One Liner GOD mode
+  // updateQuality() {
+  //   this.items.forEach(
+  //     item =>
+  //       item.name != 'Sulfuras, Hand of Ragnaros' &&
+  //       ((item.sellIn -= 1),
+  //       (item.quality = Math.max(
+  //         0,
+  //         Math.min(
+  //           50,
+  //           item.name == 'Aged Brie'
+  //             ? item.sellIn < 0
+  //               ? item.quality + 2
+  //               : item.quality + 1
+  //             : item.name == 'Backstage passes to a TAFKAL80ETC concert'
+  //             ? item.sellIn < 0
+  //               ? 0
+  //               : item.sellIn < 5
+  //               ? item.quality + 3
+  //               : item.sellIn < 10
+  //               ? item.quality + 2
+  //               : item.quality + 1
+  //             : item.sellIn < 0
+  //             ? item.quality - 2
+  //             : item.quality - 1
+  //         )
+  //       )))
+  //   )
+  //   return this.items
+  // }
 }
