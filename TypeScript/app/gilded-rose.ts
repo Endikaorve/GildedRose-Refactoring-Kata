f966b50434266aafb953496a2bc2eb00f12b46aa
Endika Orube <endikaorve@gmail.com>
2023-05-14 14:50:41 +0200

    refactor

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -50,24 +50,24 @@ const update = (item: Item) => {
   }
 
   if (item.name === ItemNames.BACKSTAGE) {
+    item.sellIn = item.sellIn - 1;
+
     if (item.quality >= 50) {
       return;
     }
 
     item.quality = item.quality + 1;
 
-    if (item.sellIn < 11) {
+    if (item.sellIn < 10) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
       }
     }
-    if (item.sellIn < 6) {
+    if (item.sellIn < 5) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
       }
     }
-
-    item.sellIn = item.sellIn - 1;
 
     if (item.sellIn >= 0) {
       return;
