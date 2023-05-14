e843d7dcac19886e98e402cdde28a428b7f577a9
Endika Orube <endikaorve@gmail.com>
2023-05-14 14:22:29 +0200

    refactor

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -30,25 +30,26 @@ const updateQuality1 = (item: Item) => {
   }
 
   if (
-    item.name != "Aged Brie" &&
-    item.name != "Backstage passes to a TAFKAL80ETC concert"
+    item.name !== "Aged Brie" &&
+    item.name !== "Backstage passes to a TAFKAL80ETC concert"
   ) {
     if (item.quality > 0) {
       item.quality = item.quality - 1;
     }
-  } else {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+    return;
+  }
+
+  if (item.quality < 50) {
+    item.quality = item.quality + 1;
+    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+      if (item.sellIn < 11) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
+      }
+      if (item.sellIn < 6) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
         }
       }
     }
